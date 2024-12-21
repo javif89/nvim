3770a31721ab9b40920ad043f40882453e6488c8
Javier Feliz <javier0eduardo@hotmail.com>
2024-12-21 14:20:54 -0500

    Initial commit

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,15 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  opts = { transparent_background = false },
+  priority = 1000,
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'catppuccin-mocha'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
+}
